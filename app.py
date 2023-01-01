64eff908b7ee4b1e0d8aa8295ab21d5ee8d37dba
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-02 03:08:00 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamilt as st
+
+from add_pages import pages
+from addpages import page1 as p1
+from addpages import page2 as p2
+from addpages import page3 as p3
+
+app - pages()
+
+
+app.add_page('에스프레소 머신 개념', p1.app)
+app.add_page('에스프레소 머신 설치', p2.app)
+app.add_page('에스프레소 머신 개념', p3.app)
+
+app.run()
