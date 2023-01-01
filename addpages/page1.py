2ceb150b3abce10a658b19098e794807ed3b33fa
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-02 04:02:19 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 def app():
   st.write("개념 수정중")
-  name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way','1way','수위감지봉','지글러','펌프','모터','콘덴서','열교환기','압력스위치'])
+  img=db('만스 머신 개념.png')
+  st.image(img,width=200)
+  name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way','1way','수위감지봉','지글러','펌프','모터','콘덴서','열교환기','압력스위치','메인보드'])
+  
