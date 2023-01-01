ae29704dbb9e48c5ac4ce2e65ad1884a236255b0
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-02 03:40:08 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def app():
   st.write("개념 수정중")
-  name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way'])
+  name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way','1way','수위감지봉','지글러','펌프','모터','콘덴서','열교환기','압력스위치'])
