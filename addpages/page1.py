39944bfb514b871499096e12d67c6cd3f653d263
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-03 01:14:52 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -3,9 +3,10 @@ import streamlit as st
 def app():
   st.write("개념 수정중")
   img='db/Mans_Machine_Concepts.png'
-  st.image(img,width=600)
-  name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way','1way','수위감지봉','지글러','펌프','모터','콘덴서','열교환기','압력스위치','메인보드'])
   
+  name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way','1way','수위감지봉','지글러','펌프','모터','콘덴서','열교환기','압력스위치','메인보드'])
+  if name == '선택하세요':
+    st.image(img,width=600)
   
   if name == '보일러':
     st.write("보일러")
