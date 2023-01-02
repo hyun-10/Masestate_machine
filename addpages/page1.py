ed6ba83153e58e6bdad45a1f1e8e7c1901185f11
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-03 01:10:03 +0900

    Update page1.py

--- addpages/page1.py
+++ addpages/page1.py
@@ -6,3 +6,6 @@ def app():
   st.image(img,width=600)
   name = st.sidebar.selectbox('자료', ['선택하세요','보일러','그룹헤드', '히팅코일','버큠벨브','3way','2way','1way','수위감지봉','지글러','펌프','모터','콘덴서','열교환기','압력스위치','메인보드'])
   
+def data(name):
+  st.write("보일러")
+    
