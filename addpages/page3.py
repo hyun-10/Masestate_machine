44aa420749234508bed73a219e29ee423ab57eff
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-03 01:43:11 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -1,7 +1,7 @@
 import streamlit as st
 def app():
   st.write("수리 수정중")
-  name = st.sidebar.selectbox('자료', ['선택하세요','가스켓 교체','샤워스크린 교체','오버홀'])
+  name = st.sidebar.selectbox('자료', ['선택하세요','가스켓 교체','샤워스크린 교체','오버홀','필터'])
   
   if name == '오버홀':
     overhaul_1 ='db/overhaul/overhaul_1.jpg'
@@ -12,3 +12,8 @@ def app():
     st.image(overhaul_2,width=300)
     st.image(overhaul_3,width=300)
     st.image(overhaul_4,width=300)
+    
+  if name == '필터':
+    names = st.sidebar.selectbox('자료', ['선택하세요','동파'])
+    if names == '동파':
+      freeze and burst_1 = 'db/freeze and burst/filter/freeze and burst_filter_1.jpg'
