41f80ee414dd86018576bb4ef83fb5a27ad3d9b7
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2023-01-03 01:27:07 +0900

    Update page3.py

--- addpages/page3.py
+++ addpages/page3.py
@@ -4,5 +4,5 @@ def app():
   name = st.sidebar.selectbox('자료', ['선택하세요','가스켓 교체','샤워스크린 교체','오버홀'])
   
   if name == '오버홀':
-    overhaul_1 ='db/overhaul/overhaul_1'
+    overhaul_1 ='db/overhaul/overhaul_1.jpg'
      st.image(overhaul_1,width=600)
